afe8be998bebfe7076a7089b8dfb809c51017905
programcion programacion <programacioncesar@hotmail.com>
2022-03-08 17:33:50 +0000

    prueba pedrote

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -6,10 +6,8 @@ namespace PruebaGit
     {
         static void Main(string[] args)
         {
-            Console.Write("hola como estas");
-            Console.WriteLine("Prueba jorge");
-            Console.WriteLine("Prueba Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
-            //Prueba Airam. uwu
+
+
         }
     }
 }
