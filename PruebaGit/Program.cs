b05163ce42c23e9c09d882713c8a41b08d4b9615
programcion programacion <programacioncesar@hotmail.com>
2022-03-08 17:16:10 +0000

    prueba maria

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -6,7 +6,10 @@ namespace PruebaGit
     {
         static void Main(string[] args)
         {
-
+            Console.Write("hola como estas");
+            Console.WriteLine("Prueba jorge");
+            Console.WriteLine("Prueba Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+            //Prueba Airam. uwu
         }
     }
 }
